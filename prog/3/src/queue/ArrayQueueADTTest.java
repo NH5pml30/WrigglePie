1caa50150bab740975086c1b076c39dae9fbe13f
NH5 <holiavin.nikolai.nh5@gmail.com>
2020-02-25 15:32:08 +0300

    Add tests

---
+++ prog/3/src/queue/ArrayQueueADTTest.java
@@ -0,0 +1,25 @@
+package queue;
+
+public class ArrayQueueADTTest {
+    public static void fill(ArrayQueueADT Queue) {
+        for (int i = 0; i < 10; i++) {
+            ArrayQueueADT.enqueue(Queue, i);
+        }
+    }
+
+    public static void dump(ArrayQueueADT Queue) {
+        while (!ArrayQueueADT.isEmpty(Queue)) {
+            System.out.println(
+                ArrayQueueADT.size(Queue) + " " +
+                ArrayQueueADT.element(Queue) + " " +
+                ArrayQueueADT.dequeue(Queue)
+            );
+        }
+    }
+
+    public static void main(String[] args) {
+        ArrayQueueADT Queue = new ArrayQueueADT();
+        fill(Queue);
+        dump(Queue);
+    }
+}
---
+++ prog/3/src/queue/ArrayQueueModuleTest.java
@@ -0,0 +1,24 @@
+package queue;
+
+public class ArrayQueueModuleTest {
+    public static void fill() {
+        for (int i = 0; i < 10; i++) {
+            ArrayQueueModule.enqueue(i);
+        }
+    }
+
+    public static void dump() {
+        while (!ArrayQueueModule.isEmpty()) {
+            System.out.println(
+                ArrayQueueModule.size() + " " +
+                ArrayQueueModule.element() + " " +
+                ArrayQueueModule.dequeue()
+            );
+        }
+    }
+
+    public static void main(String[] args) {
+        fill();
+        dump();
+    }
+}
